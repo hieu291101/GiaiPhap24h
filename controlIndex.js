15a347216087ae2ad3f8ae353bff98d7de0188d5
hieu291101 <56464164+hieu291101@users.noreply.github.com>
2021-04-17 20:36:48 +0700

    new

---
+++ controlIndex.js
@@ -0,0 +1,7 @@
+// var btn_dangNhap = document.getElementById("btn_dangNhap");
+// btn_dangNhap.onclick = function(){
+//     var edt_dangNhap = document.getElementById("username");
+//     var edt_matKhau = document.getElementById("password");
+//     if(edt_dangNhap == "admin")
+
+// }
